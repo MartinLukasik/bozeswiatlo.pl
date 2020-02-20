a3d1c44cf0754f74100aa22121c5cef2bf9bef43
mlukasik <martilukas7@gmail.com>
2020-02-20 14:04:26 +0100

    slider

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,32 +1,40 @@
-const slides = document.querySelectorAll('[class^=bg-]');
-let nextSlide = 2;
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
 
-setTimeout(changeSlide, 9000);
+const intervalTime = 9000;
+let slideInterval;
 
-function changeSlide() {
-  slides.forEach(item => {
-    if (item.className.includes(`bg-${nextSlide}`)) {
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-  nextSlide = (nextSlide % 3) + 1;
-  setTimeout(changeSlide, 9000);
-}
-
-let isAnimationPaused = false;
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY) {
-    document.querySelectorAll('[class^=bg]').forEach(item => {
-      item.classList.add('animation-paused');
-      isAnimationPaused = true;
-    });
+const nextSlide = () => {
+  const current = document.querySelector('.current');
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add('current');
   } else {
-    document.querySelectorAll('[class^=bg]').forEach(item => {
-      item.classList.remove('animation-paused');
-      isAnimationPaused = false;
-    });
+    slides[0].classList.add('current');
   }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+  const current = document.querySelector('.current');
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add('current');
+  } else {
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+// Button events
+next.addEventListener('click', () => {
+  nextSlide();
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, intervalTime);
 });
+prev.addEventListener('click', () => {
+  prevSlide();
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, intervalTime);
+});
+
+slideInterval = setInterval(nextSlide, intervalTime);
