127ed161c82720a9855554441e67b52ea4a11946
Martin Lukasik <martilukas7@gmail.com>
2020-03-05 14:34:58 +0100

    scroll bar

--- js/main-page.js
+++ js/main-page.js
@@ -5,3 +5,19 @@ import './modules/resonsive-nav';
 import './modules/aos';
 import './modules/bible-api';
 import './modules/background-animation';
+
+const cards = document.querySelectorAll('.m-card');
+
+cards.forEach(card => addListeners(card));
+
+function addListeners(card) {
+  card.addEventListener('mouseenter', () =>
+    setTimeout(() => card.classList.add('scroll-y'), 150)
+  );
+  card.addEventListener('mouseleave', () =>
+    setTimeout(() => {
+      card.classList.remove('scroll-y');
+      card.scrollTop = 0;
+    }, 200)
+  );
+}
