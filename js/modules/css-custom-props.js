09c2ffbd0ec09faebcf519efec9225842693df07
Martin Lukasik <martilukas7@gmail.com>
2020-03-17 10:41:36 +0100

    build

--- js/modules/css-custom-props.js
+++ js/modules/css-custom-props.js
@@ -1,18 +1,13 @@
 import debounce from 'lodash.debounce';
 
 function setCssProp() {
-  if (!window.screenY) {
-    document.documentElement.style.setProperty(
-      '--vh',
-      `${window.innerHeight * 0.01}px`
-    );
-  }
+  document.documentElement.style.setProperty(
+    '--vh',
+    `${window.innerHeight * 0.01}px`
+  );
 }
 
-document.documentElement.style.setProperty(
-  '--vh',
-  `${window.innerHeight * 0.01}px`
-);
+setCssProp();
 
 window.addEventListener(
   'resize',
